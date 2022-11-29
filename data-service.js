3e6388bd4db4291d90958d0ae5207da2f4ceb90d
Mohammed Aminoor Rahman <mrahman141@myseneca.ca>
2022-11-29 13:00:22 -0500

    fixed data_service.js

--- data-service.js
+++ data-service.js
@@ -52,13 +52,11 @@ function initialize (){
 
 
     return new Promise(function (resolve, reject) {
-        // sequelize.sync().then(()=>{
-        //     resolve();
-        // }).catch(()=>{
-        //     reject("unable to sync the database");
-        // })
-        resolve();
-        // reject("unable to sync the database");
+        sequelize.sync().then(()=>{
+            resolve();
+        }).catch(()=>{
+            reject("unable to sync the database");
+        })
         });
    
 }
